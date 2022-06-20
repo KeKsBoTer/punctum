fixed average color calculation

pointnet/calc_sh.py
@@ -3,6 +3,7 @@ from plyfile import PlyData, PlyElement
 import numpy as np
 import glob
 from os.path import join, basename
+import os
 from sh import to_spherical, calc_coeficients, lm2flat_index
 from tqdm import tqdm
 
@@ -33,6 +34,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    os.makedirs(args.out_folder, exist_ok=True)
+
     device = "cuda" if args.cuda else "cpu"
 
     for filename in tqdm(
@@ -49,7 +52,9 @@ if __name__ == "__main__":
 
         cameras = unpack_data(vertex_data, ["x", "y", "z"]).to(device)
         perceived_colors = (
-            unpack_data(vertex_data, ["red", "green", "blue"]).float().to(device)
+            unpack_data(vertex_data, ["red", "green", "blue", "alpha"])
+            .float()
+            .to(device)
             / 255.0
         )
 
@@ -67,7 +72,7 @@ if __name__ == "__main__":
                 coef_data.append((l, -l + m, list(sh.numpy())))
 
         ply_sh_data = np.array(
-            coef_data, dtype=[("l", "u1"), ("m", "i1"), ("coefficients", "f4", (3,))]
+            coef_data, dtype=[("l", "u1"), ("m", "i1"), ("coefficients", "f4", (4,))]
         )
 
         sh_elm = PlyElement.describe(ply_sh_data, "sh_coefficients")

pointnet/dataset.py
@@ -12,7 +12,7 @@ from functools import lru_cache
 
 from tqdm import tqdm
 
-from pointnet.sh import lm2flat_index
+from sh import lm2flat_index
 
 
 class OctantDataset(Dataset):
@@ -23,7 +23,6 @@ class OctantDataset(Dataset):
         data_dir: str,
         sub_sample: int = None,
         preload: bool = True,
-        coef_only: bool = False,
         selected_samples: torch.LongTensor = None,
     ):
         self.data_dir = data_dir
@@ -36,7 +35,7 @@ class OctantDataset(Dataset):
         if sub_sample is not None:
             self.ply_files = np.random.choice(self.ply_files, sub_sample)
 
-        self.load_fn = self.load_coefs_only if coef_only else self.load_ply_fast
+        self.load_fn = self.load_ply
 
         # load all into ram
         if preload:
@@ -57,7 +56,7 @@ class OctantDataset(Dataset):
         pos, color = data["vertex"]
         sh_coefficients = data["sh_coefficients"]
 
-        sh_coef = torch.empty((len(sh_coefficients), 3), requires_grad=False)
+        sh_coef = torch.empty((len(sh_coefficients), 4), requires_grad=False)
 
         for (l, m, values) in sh_coefficients:
             sh_coef[lm2flat_index(l, m)] = torch.tensor(values)
@@ -82,93 +81,46 @@ class OctantDataset(Dataset):
     def __getitem__(self, idx: int) -> Tuple[Pointclouds, torch.Tensor]:
         return self.load_fn(self.ply_files[idx])
 
-    @lru_cache(maxsize=None)
-    def load_ply_fast(self, file: str):
 
-        with open(file, "rb") as f:
-            binary_data = f.read()
+class CamerasDataset(Dataset):
+    """ Dataset containing pointclouds and their respective SH coefficients"""
 
-        data = binary_data[binary_data.find(b"end_header") + len("end_header") + 1 :]
+    def __init__(
+        self, data_dir: str,
+    ):
+        self.data_dir = data_dir
+        self.ply_files = np.array(glob.glob(os.path.join(data_dir, "*.ply")))
 
-        dt_coefs = np.dtype(
-            [("l", "u1"), ("m", "i1"), ("n", "u1"), ("coefficients", "f4", (3,))]
-        )
+        self.load_fn = self.load_ply
 
-        coefs = np.frombuffer(data, dtype=dt_coefs, count=121)
+    def __len__(self):
+        return len(self.ply_files)
 
-        dt_vertex = np.dtype(
-            [
-                ("x", "f4"),
-                ("y", "f4"),
-                ("z", "f4"),
-                ("red", "B"),
-                ("green", "B"),
-                ("blue", "B"),
-                ("alpha", "B"),
-            ]
-        )
-
-        vertices = np.frombuffer(data[121 * dt_coefs.itemsize :], dtype=dt_vertex)
-
-        flat_coefs = np.empty((121, 3), dtype=np.float32)
-        for l, m, _, c in coefs:
-            flat_coefs[lm2flat_index(l, m)] = c
-
-        pos = torch.from_numpy(
-            np.stack(
-                [
-                    vertices[["x"]].astype(np.float32),
-                    vertices[["y"]].astype(np.float32),
-                    vertices[["z"]].astype(np.float32),
-                ],
-                -1,
-            )
-        )
-
-        color = torch.from_numpy(
-            np.stack(
-                [
-                    vertices[["red"]].astype("u1"),
-                    vertices[["green"]].astype("u1"),
-                    vertices[["blue"]].astype("u1"),
-                    vertices[["alpha"]].astype("u1"),
-                ],
-                -1,
-            )
-        )
-
-        pc = Pointclouds(pos.unsqueeze(0), features=color.unsqueeze(0),)
-        pc.file_names = [file]
-        return (pc, torch.from_numpy(flat_coefs))
+    def filename(self, index: int) -> str:
+        return self.ply_files[index]
 
     @lru_cache(maxsize=None)
-    def load_coefs_only(self, file: str):
+    def load_ply(self, file: str) -> Tuple[Pointclouds, torch.Tensor]:
+        _, data = io.ply_io._load_ply_raw(file, path_manager=PathManager())
+        pos, colors = data["camera"]
 
-        with open(file, "rb") as f:
-            binary_data = f.read()
+        pos = torch.from_numpy(pos)
+        colors = torch.from_numpy(colors)
 
-        data = binary_data[binary_data.find(b"end_header") + len("end_header") + 1 :]
+        vertex_pos, vertex_color = data["vertex"]
+        vertex_pos = torch.from_numpy(vertex_pos)
+        vertex_color = torch.from_numpy(vertex_color)
 
-        dt_coefs = np.dtype(
-            [("l", "u1"), ("m", "i1"), ("n", "u1"), ("coefficients", "f4", (3,))]
-        )
+        sh_coef = None
+        if "sh_coefficients" in data:
+            sh_coefficients = data["sh_coefficients"]
 
-        coefs = np.frombuffer(data, dtype=dt_coefs, count=121)
+            sh_coef = torch.empty((len(sh_coefficients), 4), requires_grad=False)
 
-        dt_vertex = np.dtype(
-            [
-                ("x", "f4"),
-                ("y", "f4"),
-                ("z", "f4"),
-                ("red", "B"),
-                ("green", "B"),
-                ("blue", "B"),
-                ("alpha", "B"),
-            ]
-        )
+            for (l, m, values) in sh_coefficients:
+                sh_coef[lm2flat_index(l, m)] = torch.tensor(values)
 
-        flat_coefs = np.empty((121, 3), dtype=np.float32)
-        for l, m, _, c in coefs:
-            flat_coefs[lm2flat_index(l, m)] = c
+        return (pos, colors, vertex_pos, vertex_color, sh_coef)
 
-        return torch.from_numpy(flat_coefs)
+    def __getitem__(self, idx: int) -> Tuple[Pointclouds, torch.Tensor]:
+        return self.load_fn(self.ply_files[idx])